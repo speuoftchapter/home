0425ac2eaeff81c602a5232d56c59eed7cbe9a91
Peter Bacalso <peterbacalso@Peters-MacBook-Pro.local>
2018-01-29 19:12:29 -0500

    removed node modules

--- node_modules/react-vertical-timeline-component/dist-modules/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-// this should be the entry point to your library
-module.exports = {
-  VerticalTimeline: require('./VerticalTimeline').default,
-  VerticalTimelineElement: require('./VerticalTimelineElement').default
-};
